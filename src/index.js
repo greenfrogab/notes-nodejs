e42353198c2b87dfe2e8a8bd4d535c4bae171d4a
Jahid Shohel <jahid.shohel@ikea.com>
2020-01-14 22:23:38 +0100

    Added docker compose and fixed passing environment variables

--- src/index.js
+++ src/index.js
@@ -19,8 +19,8 @@ let port = 3000;
 if (process.env.PORT !== undefined) {
   port = process.env.PORT;
 }
+
 console.log("port: ", port);
-
-
 app.listen(port);
-console.log("Server is running on port " + port);
+
+console.log("Server is running on port: " + port);
--- src/repository/mongodb-repository.js
+++ src/repository/mongodb-repository.js
@@ -4,7 +4,25 @@ import mongoose from "mongoose";
 export default class NotesMongoDbRepository {
 
   constructor() {
-    mongoose.connect('mongodb://localhost:27017/notes', { useNewUrlParser: true, useUnifiedTopology: true });
+    let mongoHost = "localhost";
+    if (process.env.MONGO_HOST !== undefined) {
+      mongoHost = process.env.MONGO_HOST;
+    }
+
+    let mongoPort = 27017;
+    if (process.env.MONGO_PORT !== undefined) {
+      mongoPort = process.env.MONGO_PORT;
+    }
+
+    let mongoDb = "notes";
+    if (process.env.MONGO_DATABASE !== undefined) {
+      mongoDb = process.env.MONGO_DATABASE;
+    }
+
+    let mongoConnectionUrl = "mongodb://" + mongoHost + ":" + mongoPort + "/" + mongoDb;
+    console.log("Mongo db url: ", mongoConnectionUrl);
+    
+    mongoose.connect(mongoConnectionUrl, { useNewUrlParser: true, useUnifiedTopology: true });
     this._Note = mongoose.model('Note', { text: String, status: Boolean });
   }
 
